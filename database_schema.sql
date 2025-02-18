df8024ea853cbc9648b9c28fb50ef10814ac2776
nasaraiahp <115454397+nasaraiahp@users.noreply.github.com>
2025-02-18 16:02:12 +0530

    Add generated SQL schema

--- database_schema.sql
+++ database_schema.sql
@@ -1,6 +1,6 @@
 -- schema.sql
-DROP TABLE IF EXISTS sales;
-CREATE TABLE sales (
-    product TEXT UNIQUE,  -- Added UNIQUE constraint
+CREATE TABLE IF NOT EXISTS sales (
+    product TEXT,
+    category TEXT,
     sales_quantity INTEGER
 );
